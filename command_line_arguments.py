b55f9f59c30194f2146e9f8fef0e3b3040c94db2
zulqarnain <76658396+kernel-loophole@users.noreply.github.com>
2021-09-30 10:21:59 -0400

    Add files via upload
    This simple python module tells the user how to use the terminal-specific for  Linux users. and also how to use argparse in python and string coloring.

---
+++ command_line_arguments.py
@@ -0,0 +1,22 @@
+import argparse
+from stringcolor import *
+preser=argparse.ArgumentParser()
+print(cs("   arguments","red"))
+preser.add_argument("-t",'--test',help="this is help")#postional arguments
+preser.add_argument("-v","--verbosity",choices=[0,1,2],type=int)#optional arguments
+arg=preser.parse_args()
+print(cs("HERE IS ARGUMENTS++","blue"))
+print(cs(arg.test,"green"))
+if arg.verbosity==0:
+    abc=int(input("enter your name"))
+    if abc==0:
+        print("welcome")
+    else:
+        ("hi")
+
+    print("your key  ")
+    print(cs("this is 0","red"))
+elif arg.verbosity==1:
+    print(cs("this is 1","orange"))
+else:
+    print("NOT COLORING")
